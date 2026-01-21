6f15057a78316319d88199508e6a81e86ae90ba5
KeerthanaSeshadri <keerthanaseshadri2006@gmail.com>
2026-01-21 19:37:00 +0530

    Added profile  screen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,10 +1,11 @@
 import { View, Text, Button } from 'react-native';
+import { router } from 'expo-router';
 
-export default function ProfileScreen({ navigation }) {
+export default function ProfileScreen() {
   return (
     <View>
       <Text>Profile Page</Text>
-      <Button title="Go to Settings" onPress={() => navigation.navigate('Settings')} />
+      <Button title="Go to Settings" onPress={() => router.push('/settings')} />
     </View>
   );
 }
